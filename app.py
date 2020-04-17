c4237b78cda7454b4d46adce890ad2e9054f8f19
Batmomolab <paye.ton.momo@gmail.com>
2020-04-17 11:42:34 +0200

    on y retourne

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__, template_folder="templates")
 api = Api(app)
 parser = reqparse.RequestParser()
-parser.add_argument('distance')
 parser.add_argument('devise')
+parser.add_argument('distance')
 
 prixkm = 0.25
+
 
 # Create a URL route in our application for "/"
 @app.route('/')
 def home():
     return render_template('home.html')
 
-class CalculerPrix(Resource):
+
+class CalculPrix(Resource):
     def get(self):
-        #TODO devise
+        # TODO devise
         args = parser.parse_args()
         if args['devise'] == 'Euro':
             prix = float(args['distance']) * prixkm
@@ -33,3 +35,6 @@ class CalculerPrix(Resource):
             prix = float(args['distance']) * prixkm * 116.702
             return {'prix': prix,
                     'devise': '¥'}
+
+
+api.add_resource(CalculPrix, '/calculPrix')
